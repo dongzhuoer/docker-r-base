37576e2614bc9726a3093428702dfe49fd8ac7e4
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-04-27 15:48:54 +0800

    add `.` for docker build

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 
 tag=$1
 
-docker build -t dongzhuoer/rlang:$tag -f $tag.Dockerfile
+docker build -t dongzhuoer/rlang:$tag -f $tag.Dockerfile .
 
 echo "$DOCKER_PASSWORD" | docker login -u dongzhuoer --password-stdin
 docker push dongzhuoer/rlang:$tag
